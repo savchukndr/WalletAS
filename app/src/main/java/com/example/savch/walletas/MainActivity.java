aa9cd3a521e93e79141e72b671479b490ac6608e
Andrii Savchuk <savchukndr@gmail.com>
2016-10-31 17:05:59 +0100

    added toast messages

--- app/src/main/java/com/example/savch/walletas/MainActivity.java
+++ app/src/main/java/com/example/savch/walletas/MainActivity.java
@@ -5,86 +5,77 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
+import android.view.Gravity;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     static private double controlSum = 0.0d;
+    static private Toast toast;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textViewInfo = (TextView) findViewById(R.id.amount_view);
+        textViewInfo.setText(String.format( "%.2f", controlSum));
     }
 
-    public static boolean isAdigit(String userNameString){
-        Pattern p = Pattern.compile("^[-+]?[0-9]*\\.?[0-9]+$");
-        Matcher m = p.matcher(userNameString);
-        return m.matches();
-    }
-/*
-    public boolean isAdigit(String str){
-        for(char ch: str.toCharArray()){
-            if (Character.isDigit(ch)) {
-                continue;
-            }else{
-                return false;
-            }
-        }
-        return true;
-    }
-*/
-    public void strCheck(TextView txt, String x, String y){
+    public void strCheck(TextView txt, String x, String y) {
         double delAmount;
         double addAmount;
         if (!x.isEmpty() && y.isEmpty()) {
-            if (!isAdigit(x)) {
-                txt.setText("Неверный формат ввода числа!");
-            } else {
-                addAmount = Double.parseDouble(x);
-                controlSum += addAmount;
+            addAmount = Double.parseDouble(x);
+            controlSum += addAmount;
+            txt.setText(String.format( "%.2f", controlSum));
+        }else if(x.isEmpty() && !y.isEmpty()){
+            delAmount = Double.parseDouble(y);
+            if (controlSum < delAmount){
+                toast = Toast.makeText(getApplicationContext(), "Персик ты попытался удалить слишком много",
+                        Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }else{
+                controlSum -= delAmount;
                 txt.setText(String.format( "%.2f", controlSum));
             }
-        } else if(x.isEmpty() && !y.isEmpty()){
-            if (!isAdigit(y)) {
-                txt.setText("Неверный формат ввода числа!");
-            } else {
-                delAmount = Double.parseDouble(y);
-                if (controlSum < delAmount){
-                    txt.setText("Йоу персик, меньше уже нельзя =(");
-                }else{
-                    controlSum -= delAmount;
-                    txt.setText(String.format( "%.2f", controlSum));
-                }
-            }
-        } else if(!x.isEmpty() && !y.isEmpty()){
-            if(!isAdigit(x) && !isAdigit(y)) {
-                txt.setText(x  + " и " + y + " - Не является числом...");
-            }else if(!isAdigit(y)){
-                txt.setText(y + " - Не является числом...");
-            }else if(!isAdigit(x)){
-                txt.setText(x + " - Не является числом...");
-            }else {
-                txt.setText("Йоу персик, можнотолько одно поле в один момент");
-            }
-        } else {
+        }else if(!x.isEmpty() && !y.isEmpty()){
+            toast = Toast.makeText(getApplicationContext(), "Йоу персик, можнотолько одно поле в один момент",
+                    Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }else{
             txt.setText(String.format( "%.2f", controlSum));
         }
     }
 
     public void onClick(View view) {
-        String tmpAdd;
-        String tmpDel;
-
-        EditText addEditText = (EditText) findViewById(R.id.addEditText);
-        EditText delEditText = (EditText) findViewById(R.id.delEditText);
-
-        tmpAdd = addEditText.getText().toString();
-        tmpDel = delEditText.getText().toString();
         TextView textViewInfo = (TextView) findViewById(R.id.amount_view);
+        switch (view.getId()){
+            case R.id.countButton:
+                String tmpAdd;
+                String tmpDel;
 
-        strCheck(textViewInfo, tmpAdd, tmpDel);
+                EditText addEditText = (EditText) findViewById(R.id.addEditText);
+                EditText delEditText = (EditText) findViewById(R.id.delEditText);
+
+                tmpAdd = addEditText.getText().toString();
+                tmpDel = delEditText.getText().toString();
+
+                strCheck(textViewInfo, tmpAdd, tmpDel);
+                addEditText.setText("");
+                delEditText.setText("");
+                break;
+            case R.id.nullButton:
+                controlSum = 0.0d;
+                textViewInfo.setText(String.format( "%.2f", controlSum));
+                break;
+            case R.id.aboutButton:
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
